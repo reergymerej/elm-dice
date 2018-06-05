08ee1fdabb5e10873062045028170445dadb97cc
Jeremy Greer <jeremy.greer@engageonq.com>
2018-06-05 14:20:11 -0400

    First pass at showing die face as image

--- main.elm
+++ main.elm
@@ -1,6 +1,7 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
 import Random
 
@@ -32,10 +33,78 @@ update msg model =
             ( Model newFace, Cmd.none )
 
 
+isEven : Int -> Bool
+isEven num =
+    num % 2 == 0
+
+
+getXForNum : Int -> Int
+getXForNum num =
+    case num of
+        1 ->
+            0
+
+        2 ->
+            1
+
+        3 ->
+            2
+
+        4 ->
+            2
+
+        5 ->
+            1
+
+        6 ->
+            0
+
+        _ ->
+            0
+
+
+getYForNum : Int -> Int
+getYForNum num =
+    if num < 4 then
+        0
+    else
+        1
+
+
+getBackgroundX : Model -> ( String, String )
+getBackgroundX model =
+    let
+        width =
+            130
+    in
+    ( "backgroundPositionX", toString (-width * getXForNum model.dieFace) ++ "px" )
+
+
+getBackgroundY : Model -> ( String, String )
+getBackgroundY model =
+    let
+        width =
+            130
+    in
+    ( "backgroundPositionY", toString (-width * getYForNum model.dieFace) ++ "px" )
+
+
 view : Model -> Html Msg
 view model =
     div []
         [ h1 [] [ text (toString model.dieFace) ]
+        , div
+            [ style
+                [ ( "backgroundImage"
+                  , "url(https://upload.wikimedia.org/wikipedia/commons/e/ec/Kismet_Die_Faces.png)"
+                  )
+                , ( "width", "133px" )
+                , ( "height", "130px" )
+                , getBackgroundX model
+                , getBackgroundY model
+                ]
+            ]
+            []
         , button [ onClick Roll ] [ text "Roll" ]
         ]
 
